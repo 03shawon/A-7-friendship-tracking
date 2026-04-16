ba63fc4756a7240fc6198219b69cb34acf4404b1
03shawon <shawoncse5089@gmail.com>
2026-04-16 21:34:21 +0600

    initial setup complete

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h2 className='font-bold text-3xl bg-gray-200 text-red-300'>hello</h2>
+    </div>
+    </>
+  )
+}
+
+export default App
